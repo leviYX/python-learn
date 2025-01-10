de4c95ab8cf1e2019bfcd4b115cdcec36bab3c90
liuwenqiang <wenqiang.liu@wenge.com>
2025-01-10 18:12:43 +0800

    python中的文件处理以及使用matplotlib图表绘制

--- project/draw/mpl_squares.py
+++ project/draw/mpl_squares.py
@@ -1,7 +1,25 @@
 import matplotlib.pyplot as plt
+# 定义图片样式
+plt.style.use('Solarize_Light2')
+# 定义折线点位
+squares = [1, 4, 9, 16, 2.5,3,12]
+# 定义x轴坐标
+input_values = [1, 2, 3, 4, 5,6,7]
 
-squares = [1, 4, 9, 16, 25,3,12]
+# 绘制打点坐标
+x_values = [1, 2, 3, 4, 7]
+y_values = [1, 4, 9, 16, 12]
 
+# 在一张图片中绘制一个或多个图表。变量fig 表示整张图片。变量ax 表示图片中的各个图表，
+# 大多数情况下要使用它，返回一个tuple2元组
 fig, ax = plt.subplots()
-ax.plot(squares)
+# 根据给定的数据以有意义的方式绘制图表
+ax.plot(input_values,squares,color='red',linewidth=2)
+ax.set_title("hello python",fontsize=24)
+ax.set_xlabel("time",fontsize=14)
+ax.set_ylabel("value",fontsize=14)
+ax.scatter(x_values, y_values, s=200)
+# 设置刻度标记的大小。
+ax.tick_params(axis='both', which='major', labelsize=14)
+# 打开Matplotlib查看器并显示绘制的图表
 plt.show()
