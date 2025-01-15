e0c0d844b6a432963c3be84e6d75fe99beaac520
liuwenqiang <wenqiang.liu@wenge.com>
2025-01-15 18:29:13 +0800

    函数lambda

--- basic-grammar/func/func-param-demo.py
+++ basic-grammar/func/func-param-demo.py
@@ -6,11 +6,21 @@ clean_ops = [print_str, str.title]
 def clean_strings(strings, ops):
     result = []
     for value in strings:
-        for function in ops:
-            value = function(value)
+        for func in ops:
+            value = func(value)
             result.append(value)
     return result
 
 strs = [' 123 ','2','3','4']
 res = clean_strings(strs, clean_ops)
+print(res)
+
+print("***********************")
+def incr(num,scale):
+    return num + scale
+# lambda
+def add2(nums,func):
+    return [func(x) for x in nums]
+
+res = add2([1,2,3,4,5],lambda x:incr(x,2))
 print(res)
